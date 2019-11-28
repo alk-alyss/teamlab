0bb596b0dde966846dbb4b55c27a362717145773
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2019-11-28 20:07:04 +0200

    integrate astar into main and draw path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys, pygame
+from astar import astar
 pygame.init()
 
 size = width, height = 601, 501
@@ -6,6 +7,7 @@ black = 0, 0, 0
 white = 255, 255, 255
 red = 255, 0, 0
 green = 0, 255, 0
+blue = 0, 0, 255
 
 screen = pygame.display.set_mode(size)
 
@@ -23,6 +25,19 @@ maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],  # 0 = free space
 startPos = (2, 1)
 endPos = (7, 8)
 
+path = astar(maze, startPos, endPos)
+print(path)
+
+if type(path) == list:
+    for step in path:
+        maze[step[0]][step[1]] = 'x'
+
+for line in maze:
+    for e in line:
+        print(e, end=' ')
+    print()
+
+
 cellSize = int(width/len(maze[0]))-1, int(height/len(maze))-1
 
 
@@ -37,7 +52,8 @@ while True:
         for j in range(len(maze[i])):
             pos = j * (cellSize[0]+1)+1, i * (cellSize[1]+1)+1
             rect = pygame.Rect(pos, cellSize)
-            if maze[i][j]: color = black
+            if maze[i][j] == 1: color = black
+            elif maze[i][j] == 'x': color = blue
             elif i == startPos[0] and j == startPos[1]: color = red
             elif i == endPos[0] and j == endPos[1]: color = green
             else: color = white 
