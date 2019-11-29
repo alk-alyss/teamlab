b6bcf7f0a0389ed1be73aa106ee4fd2849953192
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2019-11-29 13:00:50 +0200

    add start and end point to final path

--- astar.py
+++ astar.py
@@ -38,7 +38,7 @@ def astar(maze, startPos, endPos):
             while currentNode.parent is not None:
                 path.append(currentNode.pos)
                 currentNode = currentNode.parent
-            path.remove(endNode.pos)
+            path.append(startNode.pos)
             return path[::-1]
 
         # Find all the adjacent valid children to the current node
