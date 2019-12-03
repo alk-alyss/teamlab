bd1c97175b936bfcdab55c49d5407435dcf74fbf
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2019-12-03 19:03:14 +0200

    add comments + code cleanup

--- main.py
+++ main.py
@@ -4,53 +4,64 @@ from astar import astar
 pg.init()
 
 def getDimmensions():
+    '''Input dialog for determining the maze size'''
     global colorActive, colorInactive
     text = ''
     color = colorInactive
     active = False
     while True:
-        inputBox = pg.Rect(int(screen.get_width()/2-85), int(screen.get_height()/2-18), 140, 32)
+        # Create the input box
+        inputBox = pg.Rect(int(screen.get_width() / 2 - 85), int(screen.get_height() / 2 - 18), 140, 32)
+
+        # Event listener
         for event in pg.event.get():
             if event.type == pg.QUIT: sys.exit()
             elif event.type == pg.MOUSEBUTTONDOWN:
-                # If the user clicked on the input box
+                # When the input box is clicked toggle active flag
                 if inputBox.collidepoint(event.pos):
-                    # Toggle the active variable.
                     active = not active
+                # Otherwise set it to False
                 else:
                     active = False
                 # Change the current color of the input box.
                 if active: color = colorActive 
                 else: color = colorInactive
             elif event.type == pg.KEYDOWN and active:
+                # When ENTER is pressed return the inputed text if valid or diplay error message if not
                 if event.key == pg.K_RETURN or event.key == pg.K_KP_ENTER:
                     if text.isdigit():
                         return int(text)
                     else:
                         print('Invalid. Input only a whole number.')
                     text = ''
+                # When BACKSPACE is pressed erase characters from the text input
                 elif event.key == pg.K_BACKSPACE:
                     text = text[:-1]
+                # When any other character key is pressed add the character to the text input
                 else:
                     text += event.unicode
+            #  Window resizing
             elif event.type == pg.VIDEORESIZE:
                 surface = pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
 
+        # Erase screen
+        screen.fill(grey)
 
-        screen.fill((30, 30, 30))
-
-        # Render the text.
+        # Render the label and text
         textSurface = font.render(text, True, color)
         labelSurface = font.render('Input maze size', True, color)
 
-        # Resize the box if the text is too long.
+        # Resize the box if the text is too long
         inputBox.w = max(200, textSurface.get_width()+10)
 
-        # Draw label and input box
+        # Move the label and text to correct spot
         screen.blit(textSurface, (inputBox.x + 5, inputBox.y + 5))
-        screen.blit(labelSurface, (inputBox.x, inputBox.y-50))
+        screen.blit(labelSurface, (inputBox.x, inputBox.y - 50))
+        
+        # Draw input box
         pg.draw.rect(screen, color, inputBox, 2)
 
+        # Update display
         pg.display.flip()
 
 def mazeToScreen(pos):
@@ -92,6 +103,7 @@ white = 255, 255, 255
 red = 255, 0, 0
 green = 0, 255, 0
 blue = 0, 0, 255
+grey = 30, 30, 30
 colorInactive = pg.Color('lightskyblue3')
 colorActive = pg.Color('dodgerblue2')
 font = pg.font.Font(None, 32)
@@ -107,7 +119,7 @@ maze = generateMaze(mazeSize)
 startPos = None, None
 endPos = None, None
 
-# Load car image and scale
+# Load car image
 car = pg.image.load('car.png')
 car = pg.transform.scale(car, (54, 54))
 
@@ -121,38 +133,51 @@ while True:
     # Event listener
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
+        # When the left mouse button is pressed toggle the weight 
+            # for the cell the mouse is currently in between 0 and 1
+        # If the cell has any other weight clicking will make it a wall
         elif event.type == pg.MOUSEBUTTONDOWN and not started:
             if event.button == 1:
                 mazePos = screenToMaze(event.pos)
-                if maze[mazePos[0]][mazePos[1]] == 1:
+                if maze[mazePos[0]][mazePos[1]]:
                     maze[mazePos[0]][mazePos[1]] = 0
                 else:
                     maze[mazePos[0]][mazePos[1]] = 1
         elif event.type == pg.KEYDOWN:
             mousePos = pg.mouse.get_pos()
+            # When s is pressed set the starting point to the cell the mouse is currently in
             if event.key == pg.K_s and not started: startPos = screenToMaze(mousePos)
+            # When e is pressed set the end point to the cell the mouse is currently in
             elif event.key == pg.K_e and not started: endPos = screenToMaze(mousePos)
+            # When c is pressed clear the maze
             elif event.key == pg.K_c and not started:
                 maze = generateMaze(mazeSize)
                 startPos = None, None
                 endPos = None, None
+            # When r is pressed clear maze and display dialog for maze size
             elif event.key == pg.K_r and not started:
                 mazeSize = getDimmensions()
                 maze = generateMaze(mazeSize)
                 startPos = None, None
                 endPos = None, None
+            # When 0-9 is pressed (including Numpad) set weigth for the cell the mouse is currently in
+            # 0 = wall, 1-9 = increasing difficulty
             elif (event.key in range(48, 58) or event.key in range(256, 266)) and not started:
                 if event.key in range(48, 58): weight = event.key - 48
                 else: weight = event.key - 256
                 mazePos = screenToMaze(mousePos)
                 setWeight(mazePos, weight)
+            # When ENTER is pressed start the navigation
             elif event.key == pg.K_RETURN and not started:
                 path = astar(maze, startPos, endPos)
                 started = True
+            # When the navigation is over pressing ENTER enables editing
             elif (event.key == pg.K_RETURN or event.key == pg.K_KP_ENTER) and finnished:
                 started = finnished = False
                 p = 0
+            # Pressing ESC closes the window
             elif event.key == pg.K_ESCAPE: sys.exit()
+        #  Window resizing
         elif event.type == pg.VIDEORESIZE:
             surface = pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
 
@@ -161,7 +186,6 @@ while True:
 
     # Draw the grid
     grid = []
-
     for i in range(len(maze)):
         grid.append([])
         for j in range(len(maze[i])):
@@ -180,7 +204,6 @@ while True:
     if started:
         # Draw the path
         newPath = []
-
         for point in path:
             y = int(point[0] * cellSize[1] + cellSize[1]/2)
             x = int(point[1] * cellSize[0] + cellSize[0]/2)
@@ -189,7 +212,7 @@ while True:
 
         drawPath = pg.draw.lines(screen, blue, False, newPath, 3)
 
-        # Draw the car
+        # Draw/animate the car
         try:
             newPos = mazeToScreen(path[p])
             if startPos[1] < endPos[1] and flipped: flip()
