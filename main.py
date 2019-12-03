df4e4692f2fe789548adf0494833196370f72ef0
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2019-12-03 18:30:13 +0200

    make window resizable + code cleanup

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def mazeToScreen(pos):
 def screenToMaze(pos):
     '''Take coordinates from the screen and convert it to a point on the maze'''
     global cellSize
-    mazePosX = int(mousePos[0] / cellSize[0])
-    mazePosY = int(mousePos[1] / cellSize[1])
+    mazePosX = int(pos[0] / cellSize[0])
+    mazePosY = int(pos[1] / cellSize[1])
     return mazePosY, mazePosX
 
 def setWeight(pos, weight):
@@ -37,8 +37,6 @@ def flip():
     flipped = not flipped
 
 # Global variables
-size = width, height = 601, 501
-
 black = 0, 0, 0
 white = 255, 255, 255
 red = 255, 0, 0
@@ -46,24 +44,20 @@ green = 0, 255, 0
 blue = 0, 0, 255
 colorInactive = pg.Color('lightskyblue3')
 colorActive = pg.Color('dodgerblue2')
-
 font = pg.font.Font(None, 32)
 
-inputBox = pg.Rect(int(width/2-85), int(height/2-18), 140, 32)
+# Pygame window creation
+screen = pg.display.set_mode((601, 501), pg.RESIZABLE)
 
+text = ''
 color = colorInactive
 active = False
-text = ''
 done = False
-
-
-# Pygame window creation
-screen = pg.display.set_mode(size)
-
 while not done:
+    inputBox = pg.Rect(int(screen.get_width()/2-85), int(screen.get_height()/2-18), 140, 32)
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
-        if event.type == pg.MOUSEBUTTONDOWN:
+        elif event.type == pg.MOUSEBUTTONDOWN:
             # If the user clicked on the input box
             if inputBox.collidepoint(event.pos):
                 # Toggle the active variable.
@@ -73,7 +67,7 @@ while not done:
             # Change the current color of the input box.
             if active: color = colorActive 
             else: color = colorInactive
-        if event.type == pg.KEYDOWN and active:
+        elif event.type == pg.KEYDOWN and active:
             if event.key == pg.K_RETURN or event.key == pg.K_KP_ENTER:
                 if text.isdigit():
                     mazeSize = int(text)
@@ -85,6 +79,9 @@ while not done:
                 text = text[:-1]
             else:
                 text += event.unicode
+        elif event.type == pg.VIDEORESIZE:
+            surface = pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
+
 
     screen.fill((30, 30, 30))
 
@@ -93,8 +90,7 @@ while not done:
     labelSurface = font.render('Input maze size', True, color)
 
     # Resize the box if the text is too long.
-    boxWidth = max(200, textSurface.get_width()+10)
-    inputBox.w = boxWidth
+    inputBox.w = max(200, textSurface.get_width()+10)
 
     # Draw label and input box
     screen.blit(textSurface, (inputBox.x + 5, inputBox.y + 5))
@@ -105,17 +101,13 @@ while not done:
 
 
 # Generate maze
-# mazeSize = 10
 maze = generateMaze(mazeSize)
 
 # Generate empty start and end positions
 startPos = None, None
 endPos = None, None
 
-# Size of each cell in the grid
-cellSize = int(width/len(maze[0])), int(height/len(maze))
-
-# Car
+# Load car image and scale
 car = pg.image.load('car.png')
 car = pg.transform.scale(car, (54, 54))
 
@@ -123,13 +115,15 @@ p = 0
 started = finnished = False
 flipped = False
 while True:
+    # Size of each cell in the grid
+    cellSize = int(screen.get_width()/len(maze[0])), int(screen.get_height()/len(maze))
+
     # Event listener
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
         elif event.type == pg.MOUSEBUTTONDOWN and not started:
-            if pg.mouse.get_pressed() == (True, False, False):
-                mousePos = pg.mouse.get_pos()
-                mazePos = screenToMaze(mousePos)
+            if event.button == 1:
+                mazePos = screenToMaze(event.pos)
                 if maze[mazePos[0]][mazePos[1]] == 1:
                     maze[mazePos[0]][mazePos[1]] = 0
                 else:
@@ -154,6 +148,8 @@ while True:
                 startPos = None, None
                 endPos = None, None
             elif event.key == pg.K_ESCAPE: sys.exit()
+        elif event.type == pg.VIDEORESIZE:
+            surface = pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
 
     # Erase screen
     screen.fill(black)
