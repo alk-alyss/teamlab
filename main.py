d197ce6a838f78a644e846f2fd229262b7578195
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2019-12-22 17:41:54 +0200

    add comments

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from astar import astar
 pg.init()
 
 # Global variables
-black = 0, 0, 0
-white = 255, 255, 255
-red = 255, 0, 0
-green = 0, 255, 0
-blue = 0, 0, 255
-grey = 30, 30, 30
-menuBG = 108, 169, 223
-colorInactive = black
-colorActive = white
-colorBtn = black
-colorOver = 30, 18, 138
-colorClick = 255, 255, 255
-font = pg.font.Font(None, 26)
-End = False
+black = 0, 0, 0  # Black color
+white = 255, 255, 255  # White color
+red = 255, 0, 0  # Red color
+green = 0, 255, 0  # Green color
+blue = 0, 0, 255  # Blue color
+grey = 30, 30, 30  # Grey color
+menuBG = 108, 169, 223  # Menu background
+colorInactive = black  # Inactive input color
+colorActive = white  # Active input color
+colorBtn = black  # Button color
+colorOver = 30, 18, 138  # Button hover color
+colorClick = 255, 255, 255  # Button click color
+font = pg.font.Font(None, 26)  # Default font
+End = False  # End flag
 
 class Button:
     '''Button class'''
